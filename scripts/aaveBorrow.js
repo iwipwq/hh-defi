31c047443eafa7324723ea8ed05d7d1948e5c6ae
iwipwq <iwipwq@gmail.com>
2022-07-28 23:57:21 +0900

    init repo

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,61 @@
+const { getNamedAccounts, deployments, ethers } = require("hardhat");
+const { getWeth, AMOUNT } = require("./getWeth");
+
+async function main() {
+  // the protocal treats everthing as an ERC20 token
+  await getWeth();
+  const { deployer } = await getNamedAccounts();
+  const { deploy, log } = deployments;
+  // abi, address
+  // Lending Pool Address Provider : 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+  // Lending Pool : ^
+  const lendingPool = await getLendingPool(deployer);
+  console.log(`랜딩 풀 주소 : ${lendingPool.address}`);
+
+  // deposit!
+  const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  // approve
+  await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer);
+  console.log("예치시키는중 ... ");
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
+  console.log("입금이 완료되었습니다.")
+}
+
+async function getLendingPool(account) {
+  const lendingPoolAddressesProvider = await ethers.getContractAt(
+    "ILendingPoolAddressesProvider",
+    "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+    account
+  );
+  const lendingPoolAddress =
+    await lendingPoolAddressesProvider.getLendingPool();
+  const lendingPool = await ethers.getContractAt(
+    "ILendingPool",
+    lendingPoolAddress,
+    account
+  );
+  return lendingPool;
+}
+
+async function approveErc20(
+  erc20Address,
+  spenderAddress,
+  amountToSpend,
+  account
+) {
+  const erc20Token = await ethers.getContractAt(
+    "IERC20",
+    erc20Address,
+    account
+  );
+  const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+  await tx.wait(1);
+  console.log("토큰 승인됨")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
