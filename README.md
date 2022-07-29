dbdceb8a3de7ea03c6fff8d09d0e9d39286d4467
iwipwq <iwipwq@gmail.com>
2022-07-29 23:47:09 +0900

    getUserAccountData, ETH/DAI conversion

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-1. Deposit collateral
+1. Deposit collateral ✅
 2. Borrow another asset: DAI
 3. Repay the DAI
 
-// TradeOffs
-
-Pros: Quick, easy, resemble what's on mainnet
-Cons: We need an ABI, some contracts are complex to work with
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -19,6 +19,27 @@ async function main() {
   console.log("예치시키는중 ... ");
   await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
   console.log("입금이 완료되었습니다.")
+
+  // availableBorrowsETH? What the conversion rate on DAI is?
+  // Borrow
+  // how much we have borrowed, how much we have in collateral, how much we can borrow
+  let {availableBorrowsETH, totalDebtETH} = await getBorrowUserData(lendingPool, deployer);
+  const daiPrice = await getDaiPrice();
+}
+
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt("AggregatorV3Interface","0x773616E4d11A78F511299002da57A0a94577F1f4");
+  const price = (await daiEthPriceFeed.latestRoundData())[1];
+  console.log(`1ETH당 DAI가격(DAI/ETH) : ${price}`)
+  return price
+}
+
+async function getBorrowUserData(lendingPool, account) {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } = await lendingPool.getUserAccountData(account);
+  console.log(`예치 증거금: ${totalCollateralETH}`);
+  console.log(`차입금: ${totalDebtETH}.`);
+  console.log(`대출한도: ${availableBorrowsETH}`);
+  return { totalDebtETH, availableBorrowsETH };
 }
 
 async function getLendingPool(account) {
